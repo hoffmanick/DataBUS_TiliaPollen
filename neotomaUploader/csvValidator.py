d975bdf8c0658a6a0ad74319e8abee39c16d2e98
Socorro <s.dominguez@ht-data.com>
2023-05-10 13:50:14 +0900

    unnested the yml file, updated the csvValidator

--- neotomaUploader/csvValidator.py
+++ neotomaUploader/csvValidator.py
@@ -1,7 +1,33 @@
 import yaml
 from yaml.loader import SafeLoader
 import pandas as pd
+import os
+import sys
+import argparse
+"""
+To run from command line use:
+python csvValidator.py /path/to/directory
+Example:
+python csvValidator.py --path=/Users/haritoto/Desktop/UWisconsin/neotoma/data --template=/Users/haritoto/Desktop/UWisconsin/neotoma/SQL_Project_Repository/210Pb_Template/template.yml
+"""
+# Obtain arguments and parse them to handle command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', type=str, help='Path to the directory')
+parser.add_argument('--template', type=str, help='Template to use for evaluation')
+args = parser.parse_args()
 
+# directory argument
+if not args.path:
+    print('Specify a directory path using the --path argument')
+    sys.exit(1)
+directory = args.path
+
+if not args.template:
+    print('Specify a template path using the --template argument')
+    sys.exit(1)
+yml_file = args.template
+
+# Leave just in case we use a nested YAML
 def flatten_dict(d, parent_key='', sep='_'):
     items = []
     for k, v in d.items():
@@ -18,32 +44,49 @@ def flatten_dict(d, parent_key='', sep='_'):
             items.append((new_key, v))
     return dict(items)
 
-# Load the YAML file and flatten the dictionary
-yml_file = 'template.yml'
+# Load the yml obtained from the argsparser
 with open(yml_file) as f:
     yml_data = yaml.load(f, Loader=SafeLoader)
-flat_yml_data = flatten_dict(yml_data)
+flat_yml_data = flatten_dict(yml_data) # OK to remove for now, reconsider if we use a nested YAML 
 
 col_keys = [k for k in flat_yml_data.keys() if k.endswith('_column')]
 
 # Get the corresponding values for the column keys
 col_values = [flat_yml_data[k] for k in col_keys]
-print(len(col_values))
 
+# Open a log file
+with open('application.log', 'w', encoding = "utf-8") as log_file:
+    # Iterate over the directory's files
+    for filename in os.listdir(directory):
+        # Some files have a .log hidden extension
+        root, ext = os.path.splitext(filename)
+        # To avoid .csv being removed
+        if ext == ".csv":
+            filename = root+ext
+        else:
+            filename = root
+        log_file.write(filename + '\n')
 
-# Load the spreadsheet file
-csv_file = 'Bass 2005 SLBE.csv'
-df = pd.read_csv(csv_file)
-#df.columns = [col.replace('.', ' ') for col in df.columns]
+        try:
+            df = pd.read_csv(os.path.join(directory, filename))
+            length = len(df.columns)
+            # log_file.write(f'Number of columns is: {length}'+'\n')
+            
+            diff_col = sorted(set(col_values)-set(df.columns))
+            diff_val = sorted(set(df.columns)-set(col_values))
+ 
+            # Compare the column names with the flattened YAML keys
+            if set(df.columns) == set(col_values):
+                log_file.write("✔  The column names and flattened YAML keys match"+ '\n')
+            else:
+                log_file.write("✗  The column names and flattened YAML keys do not match"+ '\n')
+                log_file.write(f"Columns not in values: '{diff_val}'"+ '\n')
+                log_file.write(f"Values not in columns: '{diff_col}'"+ '\n')
+        
+        except Exception as e:
+            log_file.write(f"✗  Error opening file '{root}': {e}"+ '\n')
 
-print(len(df.columns))
-#print("values not in columns")
-print(sorted(set(col_values)-set(df.columns)))
-
-#print("columns not in values")
-print(sorted(set(df.columns)-set(col_values)))
-# Compare the column names with the flattened YAML keys
-if set(df.columns) == set(col_values):
-    print("The column names and flattened YAML keys match")
-else:
-    print("The column names and flattened YAML keys do not match")
+# To Do: Consider using glob
+# Question: How many log files? Should it be a logfile for csv file?
+# Question: Why do some files seem to have a hidden `.log` extension?
+# Question: Nested or not nested YAML?
