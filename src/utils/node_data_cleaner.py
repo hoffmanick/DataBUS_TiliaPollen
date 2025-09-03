9150fb106f39e02cf85370a37efc413153d5a8a5
Socorro Dominguez <seiryu88@icloud.com>
2025-09-03 11:24:12 -0700

    Fixed cleaner based on Dave's comments and new file

--- src/utils/node_data_cleaner.py
+++ src/utils/node_data_cleaner.py
@@ -21,47 +21,33 @@ data['pi'] = data['pi'].str.replace(r'(\.)\s*,\s*(?=[A-Z])', r'\1| ', regex=True
 data['pi'] = data['pi'].str.replace(r'\s+et al\.?$', '', regex=True)
 data['pi'] = data['pi'].str.replace(r'\s+and\s+', '|', regex=True)
 data['pi'] = data['pi'].str.strip()
+data['pi'] = data['pi'].str.strip("|")
 
 # Site Data
-#data['sitename'] = data['site'].fillna(data['locality'])
 data['longitude'] = round(data['londeg'] + data['lonmin'] / 60 + data['lonsec'] / 3600, 6)
 data['latitude'] = round(data['latdeg'] + data['latmin'] / 60 + data['latsec'] / 3600, 6)
-# def fill_site(x):
-#     non_null = x.dropna()
-#     if not non_null.empty:
-#         most_common = non_null.value_counts().idxmax()
-#         return pd.Series([most_common] * len(x), index=x.index)
-#     else:
-#         return x
-data['sitename'] = data['locality'].fillna('') + ' ' + data['site'].fillna('')
-#mask = data['sitename'].str.contains('unspecified|no_precise_loc', case=False, na=False)
-#data.loc[mask, 'sitename'] = data.loc[mask, 'region'].fillna(data.loc[mask, 'country'])
-
+data['sitename'] = data['locality'].fillna('') + ', ' + data['site'].fillna('')
 data['sitename'] = data['sitename'].str.strip()
-#data['sitename'] = data.groupby(['longitude', 'latitude'])['sitename'].transform(fill_site)
 data = data.assign(**{'sample_analyst': 'Horne, David',
                       'dataset_processor': 'Horne, David'})
 
 # CU
-#data['collectionunit_name'] = data['locality'].fillna('') + ' ' + data['site'].fillna('')
-
 data = data.rename(columns = {'year': 'taxonname_year',
                               'field23': 'year'})
 data['day'] = data['day'].fillna(1)
 data['collectiondate'] = pd.to_datetime(data[["year", "month", "day"]], errors='coerce').dt.date
+data["collection_date"] = data["collection_date"].dt.strftime("%Y-%m-%d")
 data = data.rename(columns={'year': 'collectionyear'})
 
 # Record Data
 data['record_number'] = 'NODE-R' + (
-    data.groupby(['longitude', 'latitude', 'sitename', 'collectionyear'], dropna=False).ngroup().add(1).astype(str)
-)
+    data.groupby(['longitude', 'latitude', 'sitename', 'collectionyear'], 
+                 dropna=False).ngroup().add(1).astype(str))
 data['handle_complete'] = data['record_number']
 
 # Taxon Data
-
 data['taxonname'] = data['taxonname'] = data['genus'].str.strip() + ' ' + data['species'].str.strip()
 data['taxonname'] = data['taxonname'].str.replace('  ', ' ')
-
 regex = r'^(\w+(?:\s+\w+)?)'
 data['taxonname'] = data['taxonname'].astype(str).str.extract(regex)
 
@@ -82,8 +68,9 @@ data.drop(columns=['artificial habitat',
 data['natural habitat'] = data['natural habitat'].str.replace(r'lake', 'lacustrine', flags=re.IGNORECASE)
 data = data.rename(columns={'natural habitat': 'habitat',
                             'name in ref': 'name_in_publication',
-                            'node full references': 'references',
-                            'age': 'age_of_waterbody'})
+                            'node full references': 'citation',
+                            'age': 'age_of_waterbody',
+                            'combo403': 'coord_validation'})
 
 # Add missing data
 data['collection_type'] = 'modern'
@@ -109,10 +96,10 @@ columns = [
     # Contacts
     'pi', 'sample_analyst', 'dataset_processor',
     # Notes
-    'zone of coll', 'comments', 'combo403'
+    'zone of coll', 'comments', 'coord_validation'
 ]
 data = data[columns]
 data.sort_values(by=['record_number'], inplace=True)
-data.to_excel('data-all/original/NODE-cleanedAug2025.xlsx', index=False)
+data.to_excel('data-all/original/NODE-cleanedSep2025.xlsx', index=False)
 
-#csv_splitter(data, params='record_number')
+csv_splitter(data, params='record_number')
